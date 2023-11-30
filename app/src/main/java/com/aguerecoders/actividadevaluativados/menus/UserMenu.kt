89084a6150d2405e298b49824641741daac223e6
Zeben <zebenzuilopezconde@gmail.com>
2023-11-30 10:58:42 +0000

    commit: commit searchBar casi terminada

--- app/src/main/java/com/aguerecoders/actividadevaluativados/menus/UserMenu.kt
+++ app/src/main/java/com/aguerecoders/actividadevaluativados/menus/UserMenu.kt
@@ -1,38 +1,53 @@
 ﻿package com.aguerecoders.actividadevaluativados.menus
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.aguerecoders.actividadevaluativados.R
 import com.aguerecoders.actividadevaluativados.services.Persistence
+
+@Composable
+fun UserMenu(navController: NavHostController) {
+    Column {
+        BarraDeBusqueda()
+        ModeloCard()
+    }
+}
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun UserMenu(navController: NavHostController) {
-
+fun BarraDeBusqueda(){
     var searchText by remember { mutableStateOf("") }
     var searchActive by remember { mutableStateOf(false) }
-    val persistence = Persistence() // Asume que tienes una instancia de Persistence
+    val persistence = Persistence()
 
     Column {
         Text(text = "Menú de usuario")
@@ -56,19 +71,59 @@ fun UserMenu(navController: NavHostController) {
             }
         ) {
             if (searchActive) {
-                val filteredBandas = persistence.bandas.filter { it.nombreBanda.contains(searchText) }
+                val filteredBandas =
+                    persistence.bandas.filter { it.nombreBanda.contains(searchText) }
                 LazyColumn {
                     items(filteredBandas) { equipo ->
-                        Button(
-                            onClick = { navController.navigate("login") }) {
-                            Text(text = equipo.nombreBanda)
+
+                        TextButton(onClick = { /*TODO*/}) {
+                            Row(verticalAlignment = Alignment.CenterVertically) {
+                                Icon(Icons.Default.Person, contentDescription = "Icono de agregar")
+                                Text(text = equipo.nombreBanda, modifier = Modifier.padding(start = 20.dp))
+                            }
                         }
                     }
                 }
             }
         }
-
     }
 }
 
-fun TextButton(content: Unit, onClick: () -> Unit, function: () -> Unit) {}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ModeloCard() {
+    var expanded by remember { mutableStateOf(false) }
+    Column {
+        Card(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            onClick = { expanded = !expanded }
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Image(painter = painterResource(id = R.drawable.luffy_removebg_preview),
+                        contentDescription = "Luffy",
+                        modifier = Modifier.size(70.dp) // Asegúrate de que el tamaño de la imagen sea consistente
+                    )
+                    Text(text = "Monkey D. Luffy",
+                        modifier = Modifier.align(Alignment.CenterVertically).padding(start = 20.dp)
+                    )
+                }
+                // Este contenido se mostrará cuando la tarjeta esté expandida
+                if (expanded) {
+                    /*TODO aqui va el ataque y todo eso*/
+                    Text(
+                        text = "Lorem ipsum dolor sit amet",
+                        modifier = Modifier.padding(top = 16.dp)
+                    )
+                }
+            }
+        }
+    }
+}
